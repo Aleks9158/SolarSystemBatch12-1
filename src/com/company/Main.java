be00547c950361bf40b0b53001c18149ba91fac0
acca5859 <acca5859@gmail.com>
2022-05-20 00:51:18 +0300

    Solar System Basic Project ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import sun.security.provider.Sun;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SolarSystem FactorX = new SolarSystem();
+        FactorX.Sun = "1";
+        FactorX.Moon = "4";
+        FactorX.Planet = "2";
+        FactorX.Star= "10000";
+
+    }
+}
